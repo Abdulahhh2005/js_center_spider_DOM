bba67f7d9950d49056257e095dda859d35ff97b6
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-09-25 13:15:41 +0300

    make build

---
+++ index.395ddcdd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),i=e.getBoundingClientRect(),l=t.getBoundingClientRect();t.style.position="absolute",t.style.top=(i.height-l.height)/2+"px",t.style.left=(i.width-l.width)/2+"px",e.append(t);
+//# sourceMappingURL=index.395ddcdd.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+// Елементи
+const field = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+// Розміри елементів
+const infoField = field.getBoundingClientRect();
+const infoSpider = spider.getBoundingClientRect();
+// Центрування павука
+spider.style.position = "absolute";
+spider.style.top = (infoField.height - infoSpider.height) / 2 + "px";
+spider.style.left = (infoField.width - infoSpider.width) / 2 + "px";
+// Додаємо павука на поле
+field.append(spider);
+
+//# sourceMappingURL=index.f75de5e1.js.map
